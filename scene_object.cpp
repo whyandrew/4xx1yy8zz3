31f0e9712b7a942de58d12d33ade31eb9f0b2d03
whyandrew <whyandrew@yahoo.com>
2013-11-18 17:08:59 -0500

    Finished unitSquare, unitSphere

--- scene_object.cpp
+++ scene_object.cpp
@@ -26,7 +26,38 @@ bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 	// HINT: Remember to first transform the ray into object space  
 	// to simplify the intersection test.
 
-	return false;
+	bool b_isHit = false;
+	Vector3D ray_dir = worldToModel * ray.dir;
+	Point3D ray_orig = worldToModel * ray.origin;
+	double t_value;
+
+	// Sqaure is on xy-plane, so z==0
+	// If ray parellel to xy-plane, then either it misses the square
+	// or it technically intersect the "thickness" of the 2D square, which will be ingored
+	if (ray_dir[2] != 0)
+	{
+		// Find ray intersection point to xy-plane
+		t_value = -ray_orig[2] / ray_dir[2];
+		if (t_value >= 0.0) 
+		{
+			Point3D intersection(t_value * ray_dir[0] + ray_orig[0], 
+				t_value * ray_dir[1] + ray_orig[1],
+				0);
+			if (abs(intersection[0]) <= 0.50 && abs(intersection[1]) <= 0.50)
+			{
+				// within +/- 0.5
+				b_isHit = true;
+				ray.intersection.t_value = t_value;
+				ray.intersection.none = false;
+				Vector3D normal = modelToWorld * Vector3D(0.0, 0.0, 1.0);
+				normal.normalize();
+				ray.intersection.normal = normal;
+				ray.intersection.point = modelToWorld * intersection;
+			}
+		}
+	}
+	
+	return b_isHit;
 }
 
 bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
@@ -40,7 +71,57 @@ bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 	//
 	// HINT: Remember to first transform the ray into object space  
 	// to simplify the intersection test.
-	
-	return false;
+	bool b_isHit = false;
+	Vector3D ray_dir = worldToModel * ray.dir;
+	Point3D ray_orig = worldToModel * ray.origin;
+	double t_value, t_value2;
+
+	// equation: point^2 = 1
+	// Solve: A*t^2 + 2B*t + C = 0, where
+	// A = d.d ; B = (ray_orig - center).d ; 
+	//C = (ray_orig - center).(ray_orig - center) - 1
+	double d_A = ray_dir.dot(ray_dir);
+	Vector3D ray_orig_vect = ray_orig - Point3D(0.0, 0.0, 0.0);
+	double d_B = ray_orig_vect.dot(ray_dir);
+	double d_C = ray_orig_vect.dot(ray_orig_vect) - 1;
+	// Calculate D = B^2 - AC; 
+	// t = -B/A +/- sqrt(D)/A
+	double d_D = d_B * d_B - d_A * d_C;
+
+	// A should not == 0, unless ray_dir is 0, which should not happen
+	if (d_D == 0.0 && d_A != 0.0)
+	{
+		// 1 intersect. 
+		b_isHit = true;
+		t_value = - d_B / d_A;
+	}
+	else if (d_D > 0.0 && d_A != 0.0)
+	{
+		// 2 intersects, have to pick correct one, t_value > t_value2
+		t_value = - (d_B / d_A ) + (sqrt(d_D) / d_A);
+		t_value2 = - (d_B / d_A ) - (sqrt(d_D) / d_A);
+		if (t_value > 0.0 && t_value2 < 0.0)
+		{
+			b_isHit = true;
+		}
+		else if (t_value2 > 0.0)
+		{
+			t_value = t_value2;
+			b_isHit = true;
+		}
+	}
+
+	// Populate ray.intersection 
+	if (b_isHit)
+	{
+		ray.intersection.none = false;
+		ray.intersection.t_value = t_value;
+		ray.intersection.point = modelToWorld * (ray_orig + (t_value * ray_dir));
+		Vector3D normal = modelToWorld * (ray.intersection.point - Point3D(0.0, 0.0, 0.0));
+		normal.normalize();
+		ray.intersection.normal = normal;
+	}
+
+	return b_isHit;
 }
 
